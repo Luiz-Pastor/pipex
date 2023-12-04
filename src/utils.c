cb45e891859e1ad80fca74988b3151ac54184727
Luiz-Pastor <luigpacr5@gmail.com>
2023-12-04 23:01:50 +0100

    Separated some used functions

---
+++ src/utils.c
@@ -0,0 +1,20 @@
+#include "../inc/pipex.h"
+
+void	*free_array(char **arr)
+{
+	int	index;
+
+	index = 0;
+	if (!arr)
+		return NULL;
+	while (arr[index])
+		free(arr[index++]);
+	free(arr);
+	return NULL;
+}
+
+void	exit_error()
+{
+	perror("Error");
+	exit(1);
+}
